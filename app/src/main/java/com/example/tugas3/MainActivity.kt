c0bd6bdd6d15c8606dd072f7ff69c313b76ef748
alfonsrannu <alfons22411035@gmail.com>
2025-03-19 19:44:55 +0900

    initial commit

--- app/src/main/java/com/example/tugas3/MainActivity.kt
+++ app/src/main/java/com/example/tugas3/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.tugas3
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -20,7 +23,24 @@ class MainActivity : AppCompatActivity() {
 
         binding.btn2.setOnClickListener {
             Toast.makeText(this, binding.editMessage.text.toString(), Toast.LENGTH_SHORT).show()
-
         }
+
+        val editText: EditText = findViewById(R.id.editMessage)
+        editText.setOnFocusChangeListener { _, hasFocus ->
+            if (hasFocus) {
+                editText.setText("") // Hapus teks saat fokus
+            }
+        }
+
+
+        val logoutText: TextView = findViewById(R.id.LogOuttext)
+        logoutText.setOnClickListener {
+            val intent = Intent(this, Login::class.java)
+            startActivity(intent)
+
+            // Opsional: Menutup MainActivity jika logout
+            finish()
+        }
+
     }
 }
